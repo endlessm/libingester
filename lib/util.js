endlessm/libingester

commit 094ee55389ac43089863a265a7dbf5dd2630a482

util: Atomatically paginate Wordpress feeds
Also fix bugs in pagination: arrays respecting MAX_DAYS_OLD only for
the first item.  Added unit tests.  Also added a number of dev
dependencies to stub the test feeds:

- sinon and rewire in combination to stub the _fetch_rss_json()
  private function.

- sinon.useFakeTimers() to stub the global Date object.

- json-date-parser to obtain Date objects when parsing the json file.

https://phabricator.endlessm.com/T21642

lib/util.js
@@ -398,6 +398,16 @@ function _fetch_rss_json (feed_url) {
     });
 }
 
+function _is_wordpress_feed(feed_json) {
+    const has_generator = feed_json.meta.generator &&
+          feed_json.meta.generator.startsWith('https://wordpress.org/');
+    const has_xmlns = feed_json.meta['rss:site'] &&
+          feed_json.meta['rss:site']['@'] &&
+          feed_json.meta['rss:site']['@']['xmlns'] &&
+          feed_json.meta['rss:site']['@']['xmlns'].startsWith('com-wordpress');
+    return has_generator || has_xmlns;
+}
+
 function _fetch_rss_json_retrying(feed_url, attempt=1) {
     return _fetch_rss_json(feed_url).catch(err => {
         if (attempt > MAX_RETRIES) {
@@ -418,21 +428,24 @@ function _fetch_rss_json_retrying(feed_url, attempt=1) {
     });
 }
 
-
 function _fetch_rss_page (feed, page, items, all_items, max_items, oldest_date) {
-    let feed_obj, is_paginated;
-    if (typeof feed === 'function') {
-        is_paginated = true;
-        feed_obj = feed(page);
-    } else {
-        is_paginated = false;
-        feed_obj = feed;
+    let is_paginated = false;
+    let feed_url;
+
+    let feed_obj = feed;
+    if (Array.isArray(feed)) {
+        feed_obj = feed[0];
     }
 
-    let feed_url = feed_obj, is_array = false;
-    if (Array.isArray(feed_obj)) {
-        is_array = true;
-        feed_url = feed_obj.shift();
+    if (typeof feed_obj === 'function') {
+        is_paginated = true;
+        feed_url = feed_obj(page);
+    } else {
+        feed_url = feed_obj;
+    }
+
+    if (!feed_url) {
+        return Array.from(all_items);
     }
 
     logger.debug(`fetching RSS ${feed_url}`);
@@ -443,22 +456,53 @@ function _fetch_rss_page (feed, page, items, all_items, max_items, oldest_date) 
 
         // If we've run into articles which are too old, or we've hit the max
         // number of items, cease crawling
-        const done_crawling = (limited_items.length < feed_json.items.length);
+        const done_crawling = (feed_json.items.length === 0 ||
+                               limited_items.length < feed_json.items.length);
 
-        if ((is_paginated || is_array) && !done_crawling) {
-            const new_all_items = new Set(function* () { yield* all_items; yield* new_items.map(item => item.link); }());
+        const new_all_items = new Set(function* () { yield* all_items; yield* new_items.map(item => item.link); }());
 
-            if (is_paginated)
-                return _fetch_rss_page(feed, page + 1, new_items, new_all_items, max_items, oldest_date);
-            else
-                return _fetch_rss_page(feed, page, new_items, new_all_items, max_items, oldest_date);
-        } else {
-            return new_items;
+        if ((typeof feed_obj !== 'function') && _is_wordpress_feed(feed_json)) {
+            logger.debug('Wordpress feed found, adding pagination');
+            is_paginated = true;
+            const new_feed = _do_create_wordpress(feed_obj);
+            if (Array.isArray(feed)) {
+                feed.shift();
+                feed = [new_feed].concat(feed);
+            } else {
+                feed = new_feed;
+            }
         }
+
+        const _continue = Array.isArray(feed) ||
+              (!done_crawling && is_paginated);
+
+        if (!_continue) {
+            return Array.from(new_all_items);
+        }
+
+        let new_page;
+        if (done_crawling && is_paginated && Array.isArray(feed)) {
+            feed.shift();
+            new_page = 1;
+        } else {
+            if (is_paginated) {
+                new_page = page + 1;
+            } else if (Array.isArray(feed)) {
+                feed.shift();
+                new_page = 1;
+            }
+        }
+        return _fetch_rss_page(feed, new_page, new_items, new_all_items, max_items, oldest_date);
     });
 }
 
 exports.fetch_rss_entries = fetch_rss_entries;
+
+function _do_create_wordpress (feed) {
+    return function (page_num) {
+        return `${feed}?paged=${page_num}`;
+    };
+}
 
 /**
  * A utility to create a pagination function for Wordpress-generated feeds,
@@ -471,13 +515,10 @@ exports.fetch_rss_entries = fetch_rss_entries;
  * @memberof util
  */
 function create_wordpress_paginator (feed) {
-    return function (page_num) {
-        if (Array.isArray(feed)) {
-            return feed.map(uri => `${uri}?paged=${page_num}`);
-        } else {
-            return `${feed}?paged=${page_num}`;
-        }
-    };
+    if (Array.isArray(feed)) {
+        return feed.map(uri => _do_create_wordpress(uri));
+    }
+    return _do_create_wordpress(feed);
 }
 
 exports.create_wordpress_paginator = create_wordpress_paginator;

test/lib/util.test.js
@@ -1,11 +1,35 @@
 'use strict';
 
 const expect = require('chai').expect;
+const sinon = require('sinon');
 const fs = require('fs');
 const cheerio = require('cheerio');
 
-const util = require('../../lib/util');
+const rewire = require('rewire');
+const util = rewire('../../lib/util');
 const libingester = require('../../lib/index');
+const { jsonDateParser } = require('json-date-parser');
+
+const FAKE_NOW = 1523065866010;  // 2018-04-07T01:51:06.010Z
+
+const _RSS_JSON = fs.readFileSync(__dirname + '/test_files/rss-feeds.json');
+
+const FEEDS = JSON.parse(_RSS_JSON, jsonDateParser);
+
+const FEED_PAGES = [
+    ['http://simple-site.com/rss', FEEDS['simple']],
+    ['https://simple2.com/rss', FEEDS['simple_2']],
+    ['http://feed-with-dups', FEEDS['dups']],
+    ['http://my-site.com/rss', FEEDS['A_page_1']],
+    ['http://my-site.com/rss?paged=1', FEEDS['A_page_1']],
+    ['http://my-site.com/rss?paged=2', FEEDS['A_page_2']],
+    ['http://my-site.com/rss?paged=3', FEEDS['A_page_3']],
+    ['http://my-site.com/rss?paged=4', FEEDS['A_page_4']],
+    ['http://another.com/feed', FEEDS['B_page_1']],
+    ['http://another.com/feed?paged=1', FEEDS['B_page_1']],
+    ['http://another.com/feed?paged=2', FEEDS['B_page_2']],
+    ['http://another.com/feed?paged=3', FEEDS['B_page_3']],
+];
 
 describe('encode_uri', function() {
     it('encodes URIs correctly', function() {
@@ -197,5 +221,134 @@ describe('get_embedded_video_asset', () => {
 
         const video_job_ids = $(videoLinkSelector).map((i, v) => v.attribs['data-libingester-asset-id']).get();
         expect(video_job_ids).to.deep.equal([iframeAsset.asset_id, videoAsset.asset_id]);
+    });
+});
+
+describe('test_fetch_rss_entries', () => {
+    let feed_array;
+    let feed_paginated;
+    let feed_wordpress;
+    let feed_array_paged;
+    let feed_array_paged2;
+    let feed_array_mixed;
+    let feed_array_mixed_wordpress;
+    let fetch_rss_restore;
+    let clock;
+
+    beforeEach(function() {
+        clock = sinon.useFakeTimers(FAKE_NOW);
+        feed_array = ['http://simple-site.com/rss', 'https://simple2.com/rss'];
+        feed_paginated = util.create_wordpress_paginator('http://my-site.com/rss');
+        feed_wordpress = 'http://another.com/feed';
+        feed_array_paged = util.create_wordpress_paginator(['http://my-site.com/rss',
+                                                            'http://another.com/feed']);
+        feed_array_paged2 = [util.create_wordpress_paginator('http://my-site.com/rss'),
+                             util.create_wordpress_paginator('http://another.com/feed')];
+        feed_array_mixed = ['http://simple-site.com/rss',
+                            util.create_wordpress_paginator('http://another.com/feed')];
+        feed_array_mixed_wordpress = ['http://simple-site.com/rss',
+                                      'http://another.com/feed'];
+
+        const stub_fetch =  sinon.stub();
+
+        FEED_PAGES.forEach(([input, output]) => {
+            stub_fetch.withArgs(input).resolves(output);
+        });
+
+        fetch_rss_restore = util.__set__('_fetch_rss_json', stub_fetch);
+    });
+    afterEach(() => {
+        fetch_rss_restore();
+        clock.restore();
+    });
+    it('works with default settings', () => {
+        return util.fetch_rss_entries('http://simple-site.com/rss').then(items => {
+            expect(items.length).to.equal(2);
+        });
+    });
+    it('removes duplicated URLs', () => {
+        return util.fetch_rss_entries('http://feed-with-dups').then(items => {
+            expect(items.length).to.equal(2);
+        });
+    });
+    it('can specify max days old', () => {
+        return util.fetch_rss_entries('http://simple-site.com/rss', Infinity, 2).then(items => {
+            expect(items.length).to.equal(3);
+        });
+    });
+    it('can specify max items', () => {
+        return util.fetch_rss_entries('http://simple-site.com/rss', 2).then(items => {
+            expect(items.length).to.equal(2);
+        });
+    });
+    it('can specify max items and max days old', () => {
+        return util.fetch_rss_entries('http://simple-site.com/rss', 4, 2).then(items => {
+            expect(items.length).to.equal(3);
+        });
+    });
+    it('works with array, default settings', () => {
+        return util.fetch_rss_entries(feed_array).then(items => {
+            expect(items.length).to.equal(5);
+        });
+    });
+    it('can specify max days old, array', () => {
+        return util.fetch_rss_entries(feed_array, Infinity, 2).then(items => {
+            expect(items.length).to.equal(7);
+        });
+    });
+    it('can specify max items and max days old, array', () => {
+        return util.fetch_rss_entries(feed_array, 4, 2).then(items => {
+            expect(items.length).to.equal(4);
+        });
+    });
+    it('can add pagination', () => {
+        return util.fetch_rss_entries(feed_paginated, Infinity, 100).then(items => {
+            expect(items.length).to.equal(10);
+        });
+    });
+    it('can specify max items, pagination', () => {
+        return util.fetch_rss_entries(feed_paginated, 5, 365).then(items => {
+            expect(items.length).to.equal(5);
+        });
+    });
+    it('can specify max days old, pagination', () => {
+        return util.fetch_rss_entries(feed_paginated, 100, 6).then(items => {
+            expect(items.length).to.equal(7);
+        });
+    });
+    it('can add pagination to arrays', () => {
+        return util.fetch_rss_entries(feed_array_paged).then(items => {
+            expect(items.length).to.equal(4);
+        });
+    });
+    it('can add pagination to items in arrays', () => {
+        return util.fetch_rss_entries(feed_array_paged2).then(items => {
+            expect(items.length).to.equal(4);
+        });
+    });
+    it('can autodiscover wordpress pagination', () => {
+        return util.fetch_rss_entries(feed_wordpress, Infinity, 365).then(items => {
+            expect(items.length).to.equal(6);
+        });
+    });
+    it('can mix URIs and paginated in arrays', () => {
+        return util.fetch_rss_entries(feed_array_mixed, Infinity, 365).then(items => {
+            expect(items.length).to.equal(10);
+        });
+    });
+    it('can mix and autodiscover wordpress', () => {
+        return util.fetch_rss_entries(feed_array_mixed_wordpress, Infinity, 365).then(items => {
+            expect(items.length).to.equal(10);
+        });
+    });
+    it('can mix and autodiscover and specify max days old', () => {
+        return util.fetch_rss_entries(feed_array_mixed_wordpress, Infinity, 2).then(items => {
+            expect(items.length).to.equal(7);
+        });
+    });
+    it('can mix and autodiscover and specify max items', () => {
+        return util.fetch_rss_entries(feed_array_mixed_wordpress, 5, 2).then(items => {
+            expect(items.length).to.equal(5);
+        });
     });
 });

commit 9ae53259f6bb5e23c1cfbbdf1edcc07f5cdb1ca9

util: Fix RSS pagination, return links not items
Bug introduced in 094ee553.  Added unit test too.

lib/util.js
@@ -431,7 +431,7 @@ function _fetch_rss_json_retrying(feed_url, attempt=1) {
     });
 }
 
-function _fetch_rss_page (feed, page, items, all_items, max_items, oldest_date) {
+function _fetch_rss_page (feed, page, items, all_links, max_items, oldest_date) {
     let is_paginated = false;
     let feed_url;
 
@@ -448,21 +448,28 @@ function _fetch_rss_page (feed, page, items, all_items, max_items, oldest_date) 
     }
 
     if (!feed_url) {
-        return Array.from(all_items);
+        return items;
     }
 
     logger.debug(`fetching RSS ${feed_url}`);
     return _fetch_rss_json_retrying(feed_url).then(feed_json => {
-        const recent_enough_items = feed_json.items.filter(item => item.pubdate >= oldest_date && !all_items.has(item.link));
-        const limited_items = recent_enough_items.slice(0, max_items - items.length);
+        const recent_enough_items = feed_json.items
+              .filter(item => item.pubdate >= oldest_date);
+
+        const limited_items = [];
+        recent_enough_items.slice(0, max_items - items.length).forEach(item => {
+            if (!all_links.has(item.link)) {
+                limited_items.push(item);
+                all_links.add(item.link);
+            }
+        });
+
         const new_items = items.concat(limited_items);
 
         // If we've run into articles which are too old, or we've hit the max
         // number of items, cease crawling
         const done_crawling = (feed_json.items.length === 0 ||
                                limited_items.length < feed_json.items.length);
-
-        const new_all_items = new Set(function* () { yield* all_items; yield* new_items.map(item => item.link); }());
 
         if ((typeof feed_obj !== 'function') && _is_wordpress_feed(feed_json)) {
             logger.debug('Wordpress feed found, adding pagination');
@@ -480,7 +487,7 @@ function _fetch_rss_page (feed, page, items, all_items, max_items, oldest_date) 
               (!done_crawling && is_paginated);
 
         if (!_continue) {
-            return Array.from(new_all_items);
+            return new_items;
         }
 
         let new_page;
@@ -495,7 +502,7 @@ function _fetch_rss_page (feed, page, items, all_items, max_items, oldest_date) 
                 new_page = 1;
             }
         }
-        return _fetch_rss_page(feed, new_page, new_items, new_all_items, max_items, oldest_date);
+        return _fetch_rss_page(feed, new_page, new_items, all_links, max_items, oldest_date);
     });
 }
 

test/lib/util.test.js
@@ -264,6 +264,8 @@ describe('test_fetch_rss_entries', () => {
     it('works with default settings', () => {
         return util.fetch_rss_entries('http://simple-site.com/rss').then(items => {
             expect(items.length).to.equal(2);
+            expect(items[0]).to.include.keys('link');
+            expect(items[1]).to.include.keys('link');
         });
     });
     it('removes duplicated URLs', () => {