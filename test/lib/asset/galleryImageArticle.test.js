b784dc2aa0989c9a7b129399b494b5a55cd799db
Manuel Quiñones <manuel.por.aca@gmail.com>
2018-05-07 11:23:20 -0300

    Multiple metadata at once for the rest of the assets
    GalleryImageArticle, GalleryVideoArticle, DictionaryAsset.
    
    - Make galleryImageArticle and galleryVideoArticle inherit from
      blogArticle.
    
    - Added testcases.

---
+++ test/lib/asset/galleryImageArticle.test.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const expect = require('chai').expect;
+const proxyquire = require('proxyquire');
+
+const verifier = require('../../../lib/asset/verifier');
+
+const mockVerifier = {
+    verify_metadata: () => {
+        return null;
+    },
+    verify_manifest_entry: () => {
+        return null;
+    },
+    VerificationError: verifier.VerificationError,
+};
+
+const libingester = proxyquire('../../../lib/index', {
+    './asset/verifier': mockVerifier,
+});
+
+const expectedHatchMetadata = {
+    'objectType': 'ArticleObject',
+    'contentType': 'text/html',
+
+    'canonicalURI': 'https://www.example.com/',
+    'matchingLinks': ['https://www.example.com/'],
+
+    'temporalCoverage': [new Date(1492545280000)],
+    'title': 'Test Asset',
+    'license': 'Proprietary',
+    'tags': [ 'some', 'tags', 'EknStaticTag' ],
+    'synopsis': 'a long time ago...',
+    'lastModifiedDate': '2017-04-18T19:54:40.000Z',
+    'revisionTag': '2017-04-18T19:54:40.000Z',
+
+    'authors': ['Coco'],
+    'published': '2017-04-18T19:54:40.000Z',
+};
+
+describe('GalleryImageArticle', () => {
+    it('can serialize out correctly', () => {
+        const asset = new libingester.GalleryImageArticle();
+        asset.set_title('Test Asset');
+        asset.set_license('Proprietary');
+        asset.set_canonical_uri('https://www.example.com/');
+        asset.set_last_modified_date(new Date(1492545280000));
+        asset.set_synopsis('a long time ago...');
+        asset.set_body('<h1>Word of the Day</h1>');
+        asset.set_author('Coco');
+        asset.set_date_published(new Date(1492545280000));
+        asset.set_read_more_text('More!');
+        asset.set_tags([ 'some', 'tags' ]);
+        asset.set_temporal_coverage([new Date(1492545280000)]);
+        asset.set_as_static_page();
+
+        asset.render();
+
+        const hatchMetadata = asset.to_metadata();
+
+        delete hatchMetadata.assetID;
+
+        expect(hatchMetadata.document).to.contain('<h1>Word of the Day</h1>');
+        expect(hatchMetadata.document).to.contain('More!');
+        // Match at least one CSS rule despite no custom SCSS
+        expect(hatchMetadata.document).to.match(/<style(.|\n)*{(.|\n)*:(.|\n)*}(.|\n)*<\/style>/);
+        delete hatchMetadata.document;
+
+        expect(hatchMetadata).to.deep.eql(expectedHatchMetadata);
+    });
+
+    it('can pass metadata to constructor and set metadata at once', () => {
+        const metadata = {
+            title: 'Test Asset',
+            tags: [ 'some', 'tags' ],
+            synopsis: 'a long time ago...',
+            canonical_uri: 'https://www.example.com/',
+            license: 'Proprietary',
+        };
+
+        const asset = new libingester.GalleryImageArticle(metadata);
+
+        const moreMetadata = {
+            last_modified_date: new Date(1492545280000),
+            date_published: new Date(1492545280000),
+            author: 'Coco',
+            body: '<h1>Word of the Day</h1>',
+            read_more_text: 'More!',
+            temporal_coverage: [new Date(1492545280000)],
+            as_static_page: true,
+        };
+
+        asset.set_metadata(moreMetadata);
+
+        asset.render();
+
+        const hatchMetadata = asset.to_metadata();
+        delete hatchMetadata.assetID;
+        delete hatchMetadata.document;
+
+        expect(hatchMetadata).to.deep.eql(expectedHatchMetadata);
+    });
+});
