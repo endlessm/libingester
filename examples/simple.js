e0b1f49bc2ae9ce7215cd2f278914d7e6d55807d
Manuel Quiñones <manuel.por.aca@gmail.com>
2018-07-05 16:09:23 -0300

    simple example

---
+++ examples/simple.js
@@ -0,0 +1,21 @@
+const libingester = require('libingester');
+
+class SimpleIngester extends libingester.FeedIngester {
+    get language () {
+        return 'es';
+    }
+
+    get feedUris () {
+        return 'https://creativecommons.org/blog/feed/';
+        // return [
+        //     'http://www.prensalibre.com/rss?rss=Guatemala',
+        //     'http://www.prensalibre.com/rss?rss=Deportes',
+        //     'http://www.prensalibre.com/rss?rss=Economia',
+        //     'http://www.prensalibre.com/rss?rss=Vida',
+        //     'http://www.prensalibre.com/rss?rss=Internacional',
+        //     'http://www.prensalibre.com/smartTV/departamental.xml',
+        // ];
+    }
+}
+
+new SimpleIngester().run();
