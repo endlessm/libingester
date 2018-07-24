6d9a68c1f0d6483bd227ae8a5a4893ed475c00cc
Manuel Quiñones <manuel.por.aca@gmail.com>
2018-07-24 14:56:04 -0300

    even more generic example

--- examples/generic.js
+++ examples/generic.js
@@ -1,12 +1,5 @@
-const { WebIngester, HTMLArticleParser } = require('libingester');
-
 // Usage:
 // node generic.js --urls https://...
 
-class GenericIngester extends WebIngester {
-    get parserClass () {
-        return HTMLArticleParser;
-    }
-}
-
-new GenericIngester().run();
+const libingester = require('libingester');
+new libingester.WebIngester().run();
