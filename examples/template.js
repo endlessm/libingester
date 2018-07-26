abb379ac740f6a1236d4d198e08563406630e482
Manuel Quiñones <manuel.por.aca@gmail.com>
2018-07-26 14:14:33 -0300

    template example

---
+++ examples/template.js
@@ -0,0 +1,35 @@
+const libingester = require('libingester');
+
+class MyParser extends libingester.HTMLArticleParser {
+
+    parseTitle ($) {
+        // Parse title from $ here
+        return super.parseTitle($);
+    }
+
+    get bodyProcessors () {
+        return [
+            // Your processors here
+            ...super.bodyProcessors,
+        ];
+    }
+
+    extractBody ($) {
+        // Extract body ffrom $ here
+        return super.extractBody($);
+    }
+}
+
+class MyIngester extends libingester.WebIngester {
+    get parserClass () {
+        return MyParser;
+    }
+
+    get uriSources () {
+        return [
+            // Add URI iterators here
+        ];
+    }
+}
+
+new MyIngester(__dirname).run();
