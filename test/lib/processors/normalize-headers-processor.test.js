e2eaa0d6b252f9ca564db2a76c89a35646e05413
Manuel Quiñones <manuel.por.aca@gmail.com>
2018-09-11 09:46:54 -0300

    Test for normalize header processor

---
+++ test/lib/processors/normalize-headers-processor.test.js
@@ -0,0 +1,32 @@
+const libingester = require('../../../');
+
+const cheerio = require('cheerio-advanced-selectors').wrap(require('cheerio'));
+const fs = require('fs-extra');
+const path = require('path');
+
+async function readHtml (name) {
+    return fs.readFile(path.join(__dirname, name), 'utf8');
+}
+
+let $body;
+describe('can process headers', () => {
+    beforeAll(async () => {
+        // FIXME needed to initialize config and logger
+        // eslint-disable-next-line no-new
+        new libingester.WebIngester();
+        const html = await readHtml('normalize-headers-processor.in.html');
+        $body = cheerio.load(html).root();
+    });
+
+    test('with defaults', async () => {
+        const expected = await readHtml('normalize-headers-processor.out-1.html');
+        const result = libingester.processors.processNormalizeHeaders($body);
+        expect(result.$body.html()).toBe(expected);
+    });
+
+    test('does not pass h6', async () => {
+        const expected = await readHtml('normalize-headers-processor.out-2.html');
+        const result = libingester.processors.processNormalizeHeaders($body, { fromIndex: 5 });
+        expect(result.$body.html()).toBe(expected);
+    });
+});
