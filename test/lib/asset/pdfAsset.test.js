194ce50581daa29580a6fece6b16eda87a62c0e6
Dylan McCall <dylan@endlessm.com>
2019-04-05 16:14:43 -0700

    Add PdfAsset with tests including can_export and can_print
    The PdfAsset class is from endlessm/endless-content-factory#1607. It
    makes sense to include this asset type in libingester, along with a test
    that includes the expected case of setting the can_export and can_print
    metadata.
    
    https://phabricator.endlessm.com/T25706

---
+++ test/lib/asset/pdfAsset.test.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const expect = require('chai').expect;
+const proxyquire = require('proxyquire');
+
+const verifier = require('../../../lib/asset/verifier');
+
+const mockVerifier = {
+    verify_metadata: () => {
+        return null;
+    },
+    verify_manifest_entry: () => {
+        return null;
+    },
+    VerificationError: verifier.VerificationError,
+};
+
+const libingester = proxyquire('../../../lib/index', {
+    './asset/verifier': mockVerifier,
+});
+
+const expectedHatchMetadata = {
+    'objectType': 'ArticleObject',
+    'contentType': 'application/pdf',
+
+    'canonicalURI': 'https://www.example.com/',
+    'matchingLinks': ['https://www.example.com/'],
+
+    'title': 'Test Asset',
+    'synopsis': 'Test Asset synopsis',
+    'license': 'Proprietary',
+    'canExport': false,
+    'canPrint': true,
+    'tags': [],
+    'lastModifiedDate': '2017-04-18T19:54:40.000Z',
+    'revisionTag': '2017-04-18T19:54:40.000Z',
+};
+
+describe('PdfAsset', () => {
+    it('can serialize out correctly', () => {
+        const asset = new libingester.PdfAsset();
+        const thumbnailAsset = new libingester.PdfAsset();
+        asset.set_title('Test Asset');
+        asset.set_synopsis('Test Asset synopsis');
+        asset.set_thumbnail(thumbnailAsset);
+        asset.set_license('Proprietary');
+        asset.set_can_export(false);
+        asset.set_can_print(true);
+        asset.set_canonical_uri('https://www.example.com/');
+        asset.set_last_modified_date(new Date(1492545280000));
+        asset.set_pdf_data('application/pdf', 'qwerty');
+
+        const hatchMetadata = asset.to_hatch_metadata();
+
+        // Check that asset ID and thumbnail asset ID are passed through
+        expect(hatchMetadata.assetID).to.equal(asset.asset_id);
+        expect(hatchMetadata.thumbnail).to.equal(thumbnailAsset.asset_id);
+        // Remove the ID metadata before checking the rest
+        delete hatchMetadata.assetID;
+        delete hatchMetadata.thumbnail;
+
+        expect(hatchMetadata).to.deep.equal(expectedHatchMetadata);
+
+        const data = asset.to_data();
+        expect(data).to.equal('qwerty');
+    });
+
+    it('can pass metadata to constructor and set metadata at once', () => {
+        const metadata = {
+            synopsis: 'Test Asset synopsis',
+            license: 'Proprietary',
+            can_export: false,
+            can_print: true,
+            canonical_uri: 'https://www.example.com/',
+            last_modified_date: new Date(1492545280000),
+        };
+
+        const asset = new libingester.PdfAsset(metadata);
+
+        const moreMetadata = {
+            title: 'Test Asset',
+            pdf_data: {
+                content_type: 'application/pdf',
+                pdf_data: 'qwerty',
+            },
+        };
+
+        asset.set_metadata(moreMetadata);
+
+        const hatchMetadata = asset.to_hatch_metadata();
+        delete hatchMetadata.assetID;
+
+        expect(hatchMetadata).to.deep.equal(expectedHatchMetadata);
+
+        const data = asset.to_data();
+        expect(data).to.equal('qwerty');
+    });
+});
